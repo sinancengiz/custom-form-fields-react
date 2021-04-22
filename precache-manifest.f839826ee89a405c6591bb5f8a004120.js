e98ab5b74ec14026b6e1e5dd8bb376aa40784b15
sinan cengiz <sinancengiz_84@hotmail.com>
2021-04-21 17:13:24 -0700

    Updates

---
+++ precache-manifest.f839826ee89a405c6591bb5f8a004120.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "96509940f9e2523fe502569518f6c106",
+    "url": "./index.html"
+  },
+  {
+    "revision": "898e3abb949d94464943",
+    "url": "./static/css/main.b693b373.chunk.css"
+  },
+  {
+    "revision": "f720b531fb9123d18404",
+    "url": "./static/js/2.5686847c.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.5686847c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "898e3abb949d94464943",
+    "url": "./static/js/main.f32f2ca7.chunk.js"
+  },
+  {
+    "revision": "1539b684621298c79e5b",
+    "url": "./static/js/runtime-main.c87f11a6.js"
+  }
+]);
