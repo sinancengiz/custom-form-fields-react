8c251dc81af90535110b15b1d3d242c56b60f2b5
sinan cengiz <sinancengiz_84@hotmail.com>
2021-04-23 12:30:25 -0700

    input types changed to be corresponde with vamos model form

--- customForm/src/App.js
+++ customForm/src/App.js
@@ -4,43 +4,78 @@ import 'custom-form-fields-react/dist/index.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 const App = () => {
-  return <CustomForm formLabel="Site Form" values= {[
-    {label:"Site Name",
-    value:"value",
-    type:"text",
-    placeholder:"Enter Site Name",
-    required:true,
-    key:1
-    },
-    {label:"State",
-    value:"value",
-    type:"text",
-    placeholder:"Enter State",
-    required:false,
-    key:2
-    },
-    {label:"Range",
-    value:50,
-    type:"range",
-    placeholder:"Enter State",
-    required:false,
-    key:3
-    },
-    {label:"Check Me Out",
-    type:"checkbox",
-    value:true,
-    required:false,
-    placeholder:"check box",
-    key:4
-    },
-    {label:"Number",
-    type:"number",
-    value:1234,
-    required:false,
-    placeholder:"Enter number",
-    key:5
-    }
-  ]} />
+  return <CustomForm values= {values} modelFormItems={modelFormItems} />
 }
 
 export default App
+
+const values = {
+  8:"Select Field",
+  10:"He is jobless, rely on EDD",
+  11:"35",
+  12:"So nice farm with a lot of sheep"
+}
+
+
+var modelFormItems = [{
+    id: 7, 
+    model_form_id: 22, 
+    position: 1, 
+    type: "FormItem::Multiselect", 
+    label: "color", 
+    encrypt: true, 
+    active: true, 
+    options: ["red", "blue", "green"], 
+    required: false
+  },{
+    id: 8, 
+    model_form_id: 22, 
+    position: 1, 
+    type: "FormItem::Select", 
+    label: "honda", 
+    encrypt: false, 
+    active: true, 
+    options: ["crv", "accord", "pilot"], 
+    required: false
+  },{
+    id: 9, 
+    model_form_id: 22, 
+    position: 1, 
+    type: "FormItem::Heading", 
+    label: "information", 
+    encrypt: true, 
+    active: true, 
+    options: null, 
+    required: false
+  },{
+    id: 10, 
+    model_form_id: 22, 
+    position: 1, 
+    type: "FormItem::Line", 
+    label: "Employment", 
+    encrypt: false, 
+    active: true, 
+    options: null, 
+    required: false
+  },{
+    id: 11, 
+    model_form_id: 22, 
+    position: 1, 
+    type: "FormItem::Number", 
+    label: "Age", 
+    encrypt: true, 
+    active: true, 
+    options: null, 
+    required: false
+  },{
+    id: 12, 
+    model_form_id: 22, 
+    position: 1, 
+    type: "FormItem::Text", 
+    label: "Description", 
+    encrypt: false, 
+    active: true, 
+    options: null, 
+    required: false
+  }
+]
