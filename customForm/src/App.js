fcaaf8f3de4e8cd5509c39e02c30f31a7699f9fb
sinan cengiz <sinancengiz_84@hotmail.com>
2021-05-04 16:08:35 -0700

    before updatind from vamos

--- customForm/src/App.js
+++ customForm/src/App.js
@@ -118,96 +118,168 @@ const values = {
   14:"another new field" 
 }
 
-
-var modelFormItems = [{
-    id: 7, 
-    model_form_id: 22, 
-    position: 1, 
-    type: "FormItem::Multiselect", 
-    label: "color", 
-    encrypt: true, 
-    active: true, 
-    options: ["red", "blue", "green"], 
-    required: false
-  },{
-    id: 15, 
-    model_form_id: 22, 
-    position: 1, 
-    type: "FormItem::Multiselect", 
-    label: "color second", 
-    encrypt: true, 
-    active: true, 
-    options: ["red", "yellow", "green"], 
-    required: false
-  },{
-    id: 8, 
-    model_form_id: 22, 
-    position: 1, 
-    type: "FormItem::Select", 
-    label: "honda", 
-    encrypt: false, 
-    active: true, 
-    options: ["crv", "accord", "pilot"], 
-    required: false
-  },{
-    id: 9, 
-    model_form_id: 22, 
-    position: 1, 
-    type: "FormItem::Heading", 
-    label: "information", 
-    encrypt: true, 
-    active: true, 
-    options: null, 
-    required: false
-  },{
-    id: 10, 
-    model_form_id: 22, 
-    position: 1, 
-    type: "FormItem::Line", 
-    label: "Employment", 
-    encrypt: false, 
-    active: true, 
-    options: null, 
-    required: false
-  },{
-    id: 11, 
-    model_form_id: 22, 
-    position: 1, 
-    type: "FormItem::Number", 
-    label: "Age", 
-    encrypt: true, 
-    active: true, 
-    options: null, 
-    required: false
-  },{
-    id: 12, 
-    model_form_id: 22, 
-    position: 1, 
-    type: "FormItem::Text", 
-    label: "Description", 
-    encrypt: false, 
-    active: true, 
-    options: null, 
-    required: false
-  },{
-    id: 13, 
-    model_form_id: 22, 
-    position: 1, 
-    type: "FormItem::Text", 
-    label: "new field", 
-    encrypt: false, 
-    active: true, 
-    options: null, 
-    required: false
-  },{
-    id: 14, 
-    model_form_id: 22, 
-    position: 1, 
-    type: "FormItem::Text", 
-    label: "extra field", 
-    encrypt: false, 
-    active: true, 
-    options: null, 
-    required: false
+var modelFormItems = [
+  {
+    "key": 1,
+    "type": "checkbox-group",
+    "label": "fare",
+    "values": [
+      {
+        "value": "First Class",
+        "selected": false,
+        "label": "First class"
+      },
+      {
+        "value": "Business",
+        "selected": false,
+        "label": "Business"
+      },
+      {
+        "value": "Economy",
+        "selected": false,
+        "label": "Economy"
+      }
+    ],
+    "required": false
+  },
+  {
+    "key": 2,
+    "type": "select",
+    "label": "toyota",
+    "values": [
+      {
+        "value": null,
+        "selected": false
+      },
+      {
+        "value": null,
+        "selected": false
+      },
+      {
+        "value": null,
+        "selected": false
+      }
+    ],
+    "required": false
+  },
+  {
+    "key": 3,
+    "type": "header",
+    "label": "title",
+    "subtype": "h3",
+    "value": null
+  },
+  {
+    "key": 4,
+    "type": "text",
+    "label": "address",
+    "subtype": "text",
+    "value": null,
+    "required": false
+  },
+  {
+    "key": 5,
+    "type": "number",
+    "label": "year",
+    "value": null
+  },
+  {
+    "key": 6,
+    "type": "textarea",
+    "label": "description",
+    "value": null,
+    "required": false
   }
 ]
+// var modelFormItems = [{
+//     id: 7, 
+//     model_form_id: 22, 
+//     position: 1, 
+//     type: "FormItem::Multiselect", 
+//     label: "color", 
+//     encrypt: true, 
+//     active: true, 
+//     options: ["red", "blue", "green"], 
+//     required: false
+//   },{
+//     id: 15, 
+//     model_form_id: 22, 
+//     position: 1, 
+//     type: "FormItem::Multiselect", 
+//     label: "color second", 
+//     encrypt: true, 
+//     active: true, 
+//     options: ["red", "yellow", "green"], 
+//     required: false
+//   },{
+//     id: 8, 
+//     model_form_id: 22, 
+//     position: 1, 
+//     type: "FormItem::Select", 
+//     label: "honda", 
+//     encrypt: false, 
+//     active: true, 
+//     options: ["crv", "accord", "pilot"], 
+//     required: false
+//   },{
+//     id: 9, 
+//     model_form_id: 22, 
+//     position: 1, 
+//     type: "FormItem::Heading", 
+//     label: "information", 
+//     encrypt: true, 
+//     active: true, 
+//     options: null, 
+//     required: false
+//   },{
+//     id: 10, 
+//     model_form_id: 22, 
+//     position: 1, 
+//     type: "FormItem::Line", 
+//     label: "Employment", 
+//     encrypt: false, 
+//     active: true, 
+//     options: null, 
+//     required: false
+//   },{
+//     id: 11, 
+//     model_form_id: 22, 
+//     position: 1, 
+//     type: "FormItem::Number", 
+//     label: "Age", 
+//     encrypt: true, 
+//     active: true, 
+//     options: null, 
+//     required: false
+//   },{
+//     id: 12, 
+//     model_form_id: 22, 
+//     position: 1, 
+//     type: "FormItem::Text", 
+//     label: "Description", 
+//     encrypt: false, 
+//     active: true, 
+//     options: null, 
+//     required: false
+//   },{
+//     id: 13, 
+//     model_form_id: 22, 
+//     position: 1, 
+//     type: "FormItem::Text", 
+//     label: "new field", 
+//     encrypt: false, 
+//     active: true, 
+//     options: null, 
+//     required: false
+//   },{
+//     id: 14, 
+//     model_form_id: 22, 
+//     position: 1, 
+//     type: "FormItem::Text", 
+//     label: "extra field", 
+//     encrypt: false, 
+//     active: true, 
+//     options: null, 
+//     required: false
+//   }
+// ]
